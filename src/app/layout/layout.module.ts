d303fefe78f0404d305bb36515a66f4bf9c0a0e0
Jing Palma <jingpalma17@gmail.com>
2020-07-12 17:26:06 +0800

    Add layout module for header and footer

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { LayoutComponent } from './layout.component';
+
+@NgModule({
+  declarations: [LayoutComponent],
+  imports: [],
+  exports: [LayoutComponent],
+  providers: [],
+})
+export class LayoutModule {}
